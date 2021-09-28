3fa3fad2e59321bfc2618b631b3e5c05e8749e99
srishti-maurya <srishti22maurya@gmail.com>
2021-09-28 14:57:49 +0530

    views, texts and styles

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+/*
+text inside the views do not inherit the views style 
+text inside the text inherits the style 
+*/
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, World!</Text>
+      </View>
+
+      <View style={styles.body}>
+        <Text style={styles.boldText}>
+          Lorem ipsum <Text>test part</Text> dolor sit ame
+        </Text>
+        <Text>Lorem ipsum dolor sit ame</Text>
+        <Text>Lorem ipsum dolor sit ame</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +28,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "pink",
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  body: {
+    backgroundColor: "yellow",
+    padding: 20,
   },
 });
