70e28cd74a0d6d784f7963fc0716ed0e14554d28
srishti-maurya <srishti22maurya@gmail.com>
2021-09-28 18:14:13 +0530

    flatlist

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
 import React, { useState } from "react";
-import {
-  Button,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 /*
-text inside the views do not inherit the views style 
-text inside the text inherits the style 
+      flatview automatically has scroll view 
+      and it does not require a unique key 
+      it is prefered over scrollview bcoz on render it does not 
+      loads all the items of the list instead loads only first few and accordingly on the scroll of view 
+      comparetively scrollview renders all the items of the list at once (initially) 
+      
 */
 
 export default function App() {
-  const [name, setName] = useState("mary");
-  const [person, setPerson] = useState({ name: "mario", age: 20 });
-  const [inputName, setInputName] = useState("John");
-  const [inputAge, setInputAge] = useState("22");
   const [list, setList] = useState([
     { name: "Jack", id: "1" },
     { name: "Jacky", id: "2" },
@@ -31,55 +24,15 @@ export default function App() {
     { name: "Tim", id: "10" },
   ]);
 
-  const buttonHandler = () => {
-    setName("srishti");
-    setPerson({ name: "Jack", age: 30 });
-  };
   return (
-    // <View style={styles.container}>
-    //     <View style={styles.header}>
-    //       <Text style={styles.boldText}>Hello, World!</Text>
-    //       <Text>My name is {name}</Text>
-    //     </View>
-    //     <View style={styles.body}>
-    //       <Text>
-    //         His name is {person.name} and his age is {person.age}
-    //       </Text>
-    //       <Text style={styles.boldText}>
-    //         Lorem ipsum <Text>test part</Text> dolor sit ame
-    //       </Text>
-    //     </View>
-    //     <View style={styles.buttonContainer}>
-    //       <Button color="white" title="update state" onPress={buttonHandler} />
-    //     </View>
-    //     <View>
-    //       <Text>Enter name:</Text>
-    //       <TextInput
-    //         placeholder="eg. Jack Joe"
-    //         style={styles.input}
-    //         onChangeText={(res) => setInputName(res)}
-    //       />
-    //       <Text>Enter age:</Text>
-    //       <TextInput
-    //         placeholder="eg. 25"
-    //         style={styles.input}
-    //         onChangeText={(res) => setInputAge(res)}
-    //       />
-    //       <Text>
-    //         Name of the user is {inputName} and age is {inputAge}
-    //       </Text>
-    //     </View>
-
     <View style={styles.container}>
-      <ScrollView>
-        {list.map((obj) => (
-          <View key={obj.id}>
-            <Text style={styles.list}>{obj.name}</Text>
-          </View>
-        ))}
-      </ScrollView>
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={list}
+        renderItem={({ item }) => <Text style={styles.list}>{item.name}</Text>}
+      />
     </View>
-    // </View>
   );
 }
 
@@ -90,33 +43,11 @@ const styles = StyleSheet.create({
     // alignItems: "center",
     // justifyContent: "center",
   },
-  header: {
-    backgroundColor: "pink",
-    padding: 20,
-  },
-  boldText: {
-    fontWeight: "bold",
-  },
-  body: {
-    backgroundColor: "yellow",
-    padding: 20,
-  },
-  buttonContainer: {
-    backgroundColor: "gray",
-    marginTop: 20,
-    marginBottom: 20,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#ccc",
-    padding: 8,
-    margin: 10,
-    width: 200,
-  },
   list: {
     padding: 30,
     marginTop: 50,
     backgroundColor: "#CEE5D0",
     fontSize: 24,
+    marginHorizontal: 10,
   },
 });
