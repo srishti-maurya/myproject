3c992eae11dcb3cb0f4edb475b2a9cb411407f77
srishti-maurya <srishti22maurya@gmail.com>
2021-09-28 15:07:55 +0530

    using state

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 /*
 text inside the views do not inherit the views style 
@@ -8,18 +7,28 @@ text inside the text inherits the style
 */
 
 export default function App() {
+  const [name, setName] = useState("mary");
+  const [person, setPerson] = useState({ name: "mario", age: 20 });
+  const buttonHandler = () => {
+    setName("srishti");
+    setPerson({ name: "Jack", age: 30 });
+  };
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.boldText}>Hello, World!</Text>
+        <Text>My name is {name}</Text>
       </View>
-
       <View style={styles.body}>
+        <Text>
+          His name is {person.name} and his age is {person.age}
+        </Text>
         <Text style={styles.boldText}>
           Lorem ipsum <Text>test part</Text> dolor sit ame
         </Text>
-        <Text>Lorem ipsum dolor sit ame</Text>
-        <Text>Lorem ipsum dolor sit ame</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="update state" onPress={buttonHandler} />
       </View>
     </View>
   );
@@ -42,5 +51,9 @@ const styles = StyleSheet.create({
   body: {
     backgroundColor: "yellow",
     padding: 20,
+  },
+  buttonContainer: {
+    backgroundColor: "gray",
+    marginTop: 20,
   },
 });
